a3606d4482b5bedded15c6d27b1417c3a359fc6f
hmontesdeoca_ext@amco.mx <AmArillo4101@@>
2021-07-08 11:36:24 -0500

    arreglando problemas de identacion windows

--- services/web/entrypoint.sh
+++ services/web/entrypoint.sh
@@ -1,16 +1,11 @@
 #!/bin/sh
-
 if [ "$DATABASE" = "postgres" ]
 then
     echo "Esperando a postgres..."
-
     while ! nc -z $SQL_HOST $SQL_PORT; do
       sleep 0.1
     done
-
     echo "PostgreSQL inicio"
 fi
-
 python manage.py create_db
-
 exec "$@"
